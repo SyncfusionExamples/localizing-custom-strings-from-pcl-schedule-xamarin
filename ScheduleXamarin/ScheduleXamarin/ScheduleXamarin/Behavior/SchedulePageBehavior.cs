b64a54739b6df3d066970f3ba8fd519430452bf3
sakithya <sakithya.chidambaram@syncfusion.com>
2021-02-12 17:13:39 +0530

    Localization

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulePageBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulePageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.FindByName<SfSchedule>("schedule");
+
+            schedule.Locale = "fr";
+        }
+    }
+}
+
+       
---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ScheduleXamarin
+{
+
+    public class SchedulerViewModel
+    {
+        public ScheduleAppointmentCollection scheduleAppointmentCollection { get; set; } = new ScheduleAppointmentCollection();
+
+        public SchedulerViewModel()
+        {
+            ScheduleAppointment scheduleAppointment = new ScheduleAppointment();
+            scheduleAppointment.StartTime = new DateTime(2018, 02, 14, 12, 0, 0);
+            scheduleAppointment.EndTime = new DateTime(2018, 02, 14, 02, 0, 0);
+            scheduleAppointment.Subject = "Meeting";
+            scheduleAppointment.Location = "Chennai";
+            scheduleAppointment.IsAllDay = true;
+
+            scheduleAppointmentCollection.Add(scheduleAppointment);
+        }
+    }
+}
